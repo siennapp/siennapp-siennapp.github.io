40d951d8eb5015bc0ce892643386bb7034069a4e
siennapp <pstar224@gmail.com>
2023-03-08 00:20:47 +0900

    Updates

---
+++ component---src-pages-info-tsx-81df1fddcd0e54958590.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgatsby_starter_default=self.webpackChunkgatsby_starter_default||[]).push([[977],{8187:function(t,a,e){e.r(a);var u=e(1883),i=e(917);a.default=function(t){let{data:{site:{siteMetadata:{title:a,description:e,author:n}}}}=t;return(0,i.tZ)("div",null,(0,i.tZ)("div",null,a," ",e," ",n),(0,i.tZ)(u.Link,{to:"/"},"To Main"))}}}]);
+//# sourceMappingURL=component---src-pages-info-tsx-81df1fddcd0e54958590.js.map
